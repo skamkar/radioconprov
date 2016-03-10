06a2e14d4ca04f8756e9ae234284437063d5b629
skamkar <sjk@zestfinance.com>
2016-03-09 23:38:36 -0800

    asset mod

--- Gemfile
+++ Gemfile
@@ -11,6 +11,9 @@ gem 'bootstrap-sass', '~> 3.2.0'
 gem 'autoprefixer-rails'
 gem 'sass-rails', '~> 5.0'
 
+# for heroku asset compilation
+gem 'rails_12factor', group: :production
+
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,12 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  # HEROKU SETTINGS
+  config.serve_static_assets = true
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+  config.assets.compile = true
+
+
 end
